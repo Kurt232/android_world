1e6d306d219b64a883dfb156d2be038b5893f4ff
duwenjie <532923577@qq.com>
2024-06-29 00:59:51 +0800

    fixed: add choice and scroll action

--- android_world/agents/human_agent.py
+++ android_world/agents/human_agent.py
@@ -21,6 +21,8 @@ from android_world.agents import human_agent_utils
 from android_world.env import interface
 from android_world.env import json_action
 
+from android_world.agents.human_agent_utils import ElementTree
+
 
 class HumanAgent(base_agent.EnvironmentInteractingAgent):
   """Human agent; wait for user to indicate they are done."""
@@ -49,65 +51,16 @@ class HumanAgent(base_agent.EnvironmentInteractingAgent):
     try:
       response = int(response)
     except:
-      print('\033[1;31m' + 'Invalid id. converting to wait action.' + '\033[0m')
+      print('\033[1;31m' + 'Invalid id, replaced by wait action.' + '\033[0m')
       response = 0
-
-    action_wait = {'action_type': 'wait'}
 
     action_type = action_list[response]
     print(f'Action: {action_type}')
-    action_details = {'action_type': action_type}
 
     state = self.get_post_transition_state()
     element_tree = human_agent_utils.forest_to_element_tree(state.forest)
 
-    if action_type == "status":
-      goal_status = ['complete', 'infeassible']
-      status_id = input(f'Please input the status id in {goal_status}.')
-      try:
-        status = goal_status[int(status_id)]
-        action_details['goal_status'] = status
-      except:
-        print('\033[1;31m' + 'Invalid id. converting to wait action.' +
-              '\033[0m')
-        action_details = action_wait
-    elif action_type == "answer":
-      action_details['text'] = input('Please input the text:')
-    elif action_type in ["click", "long_press", "input_text", "scroll"]:
-      print('\033[0;32m', '-' * 40, 'State', '-' * 40, '\033[0m')
-      print(element_tree.get_str(is_color=True))
-      ele_id = input('Please input the element id:')
-      try:
-        ele_id = int(ele_id)
-        ele = element_tree.ele_map[ele_id]
-        x, y = ele.ele.bbox_pixels.center
-        x, y = int(x), int(y)
-        action_details['x'] = x
-        action_details['y'] = y
-        if action_type == "input_text":
-          action_details['text'] = input('Please input the text:')
-        elif action_type == "scroll":
-          action_details['bbox_pixels'] = ele.ele.bbox_pixels
-          direction_list = ['up', 'down', 'left', 'right']
-          direction = input(
-              f'Please input the direction id in {direction_list}:')
-          try:
-            direction = direction_list[int(direction)]
-            action_details['direction'] = direction
-          except:
-            print('\033[1;31m' + 'Invalid id. converting to wait action.' +
-                  '\033[0m')
-            action_details = action_wait
-      except:
-        print('\033[1;31m' + 'Invalid id. converting to wait action.' +
-              '\033[0m')
-        action_details = action_wait
-    elif action_type == "open_app":
-      app_name = input(
-          'Please input the app name\n(nothing will happen if the app is not installed):\n'
-      )
-      action_details['app_name'] = app_name
-
+    action_details, ele_id = self.get_action_and_id(action_type, element_tree)
     self.env.execute_action(json_action.JSONAction(**action_details))
 
     result = {}
@@ -116,7 +69,7 @@ class HumanAgent(base_agent.EnvironmentInteractingAgent):
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d_T%H%M%S')
 
     human_agent_utils.save_to_yaml(self.save_path, element_tree.str, timestamp,
-                                   action_type, action_details,
+                                   action_type, action_details, ele_id,
                                    action_details.get('text', None),
                                    self.env.device_screen_size[0],
                                    self.env.device_screen_size[1])
@@ -128,3 +81,69 @@ class HumanAgent(base_agent.EnvironmentInteractingAgent):
 
   def get_post_transition_state(self) -> interface.State:
     return self.env.get_state()
+
+  def get_action_and_id(self, action_type: str,
+                        element_tree: ElementTree) -> tuple[dict, int | None]:
+    action_details = {'action_type': action_type}
+    wait_action = {'action_type': 'wait'}
+
+    if action_type == "status":
+      goal_status = ['complete', 'infeasible']
+      status_id = input(f'Please input the status id in {goal_status}.')
+      try:
+        status = goal_status[int(status_id)]
+        action_details['goal_status'] = status
+      except:
+        print('\033[1;31m' + 'Invalid id, replaced by wait action.' + '\033[0m')
+        return wait_action, None
+
+    elif action_type == "answer":
+      action_details['text'] = input('Please input the text:')
+
+    elif action_type in ["click", "long_press", "input_text", "scroll"]:
+      print('\033[0;32m', '-' * 40, 'State', '-' * 40, '\033[0m')
+      print(element_tree.get_str(is_color=True))
+      ele_id = input('Please input the element id:')
+      try:
+        ele_id = int(ele_id)
+        ele = element_tree.ele_map[ele_id]
+      except:
+        print('\033[1;31m' + 'Invalid id, replaced by wait action.' + '\033[0m')
+        return wait_action, None
+
+      if ele.local_id is None:
+        print(
+            '\033[1;31m' +
+            f'Element: {ele_id} is NOT interacted with, replaced by wait action.'
+            + '\033[0m')
+        return wait_action, None
+
+      x, y = ele.ele.bbox_pixels.center
+      x, y = int(x), int(y)
+      action_details['x'] = x
+      action_details['y'] = y
+      if action_type in ["click", "long_press"]:
+        pass
+      elif action_type == "input_text":
+        action_details['text'] = input('Please input the text:')
+      elif action_type == "scroll":
+        action_details['index'] = ele.local_id
+        direction_list = ['up', 'down', 'left', 'right']
+        direction = input(f'Please input the direction id in {direction_list}:')
+        try:
+          direction = direction_list[int(direction)]
+          action_details['direction'] = direction
+        except:
+          print('\033[1;31m' + 'Invalid id, replaced by wait action.' +
+                '\033[0m')
+          return wait_action, None
+
+      return action_details, ele_id
+
+    elif action_type == "open_app":
+      app_name = input(
+          'Please input the app name\n(nothing will happen if the app is not installed):\n'
+      )
+      action_details['app_name'] = app_name
+
+    return action_details, None
